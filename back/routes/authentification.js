132d65533e6f6e80837ab1c704260569844394ce
Fathi <fathi.kadir.a@gmail.com>
2022-01-17 18:05:13 +0100

    finish

--- back/routes/authentification.js
+++ back/routes/authentification.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const passwordServices = require('../services/passwordServices')
 
 const User = require('../models/user.model');
+const Question = require('../models/question.model');
+
 
 // @route POST Create an User --> /users/register
 router.post('/register', (req, res) => {
@@ -21,5 +23,51 @@ router.post('/login', (req, res) => {
         })
         .catch(err => res.status(202).json({ Error: err, msg: "Incorrect" }))
 })
+
+router.get('/GetQuestionResponse/:id', (req, res) => {
+    let response = {Question: req.params.id, Answers:[]};
+    User.find().populate('answers').then(users => {
+        Question.find().populate('answerOptions').then(question => {
+            question[req.params.id-1].answerOptions.map(questionAnswer => {
+                response.Answers.push({id:questionAnswer.optionNumber.toString(), count:0})
+            })
+            users.map(user => {
+                user.answers.map(answer => {
+                    response.Answers.map(QuestionAnswer => {
+                        if(answer.questionId.valueOf() === question[req.params.id-1].id){
+                            if(answer.questionAnswer.toString() === QuestionAnswer.id){
+                                QuestionAnswer.count += 1;
+                            }
+                        }
+                    })
+                })
+            })
+            res.json(response)
+        })
+    })
+});
+
+router.get('/GetAllQuestions/', (req, res) => {
+    let resQuestions = {Question: []};
+    let id = 1;
+        Question.find().then(questions => {
+            questions.map(question => {
+                resQuestions.Question.push({id:id.toString(), Text:question.question})
+                id+=1;
+            })
+            let response = {consulations: resQuestions};
+            res.json(response)
+        })
+});
+
+router.get('/GetQuestionOptions/:id', (req, res) => {
+    let response = {Question: req.params.id, Options:[]};
+        Question.find().populate('answerOptions').then(questions => {
+            questions[req.params.id-1].answerOptions.map(answer => {
+                response.Options.push({id:answer.optionNumber.toString(), Text:answer.answerBody})
+            })
+            res.json(response)
+        })
+});
 
 module.exports = router;
